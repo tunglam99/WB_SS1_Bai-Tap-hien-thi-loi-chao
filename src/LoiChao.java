665c115c34aa25233bdc0d04e20ae71181968f4d
Nguyen Tung Lam <lamnthnue.edu.vn@gmail.com>
2019-07-23 09:53:07 +0700

    'ok'

---
+++ src/LoiChao.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class LoiChao {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhap vao ten: ");
+        String name = scanner.nextLine();
+        System.out.print("Hello " + name);
+    }
+}
